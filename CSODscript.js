de2380da3b9103cd804f0c60aab08fe25a5139a8
Peter Duong <phduong92@gmail.com>
2018-07-26 05:18:21 -0700

    Adding some validations

--- CSODscript.js
+++ CSODscript.js
@@ -13,10 +13,20 @@
 	}
 	
 	function _initData (data) {
-		// Populate Feed when document is ready
-		$(document).ready(function() {
-			_populateFeed(data);
-		});
+		if (typeof data == 'object' && data.meta.code == 200) {
+			// Populate Feed when document is ready
+			$(document).ready(function() {
+				_populateFeed(data);
+			});
+		}
+		else {
+			_handleFail();
+		}
+	}
+	
+	function _handleFail () {
+		console.log('Error: API call failed to retrieve data');
+		$('#instafeed_empty').show();
 	}
 	
 	// Populate Feed
@@ -133,15 +143,20 @@
 		
 		function _buildList (e) {
 			let ele = e.target || e,
-				val = ele.value.trim();
+				val = ele.value;
 			
 			// Clear previous autocomplete list
 			_resetList();
 			
+			let val_trimmed = val.trim();
 			// Exit if less than 3 characters 
-			if (val.length < 3) {
+			if (val_trimmed.length < 3) {
+				if (val_trimmed === '') {
+					$(inEle).val(val_trimmed);
+				}
 				return;
 			}
+
 			for (let i = 0; i < list.length; i++) {
 				if (list[i].substring(0, val.length).toUpperCase() == val.toUpperCase()) {
 					acList.push(list[i]);
@@ -178,7 +193,7 @@
 	
 	var Init = function () {
 		// Request Ajax and intialize data
-		_fetchData().then(_initData);
+		_fetchData().then(_initData).fail(_handleFail);
 		
 		var countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central Arfrican Republic","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cuba","Curacao","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Namibia","Nauro","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Korea","South Sudan","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States of America","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
 		
